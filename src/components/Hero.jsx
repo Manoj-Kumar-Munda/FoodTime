209fd5203a611b8cfab2c34c89a7d4860b648a6a
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2023-10-19 08:06:50 +0530

    search btn for sm modified

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,9 +39,9 @@ const Hero = () => {
                   </div>
 
                  
-                  <button className="inline-flex justify-center items-center rounded-r-full rounded-l-full gap-x-2 bg-primary px-4 py-3 ">
-                    <img src={search} alt="search" className='hidden sm:block' />
-                    <span className="text-white  font-Poppins">Search</span>
+                  <button className="inline-flex justify-center items-center rounded-r-full rounded-l-full gap-x-2 bg-primary px-4 py-4 sm:py-3  ">
+                    <img src={search} alt="search" className='' />
+                    <span className="text-white  font-Poppins hidden sm:block">Search</span>
                   </button>
                 </div>
               </div>
