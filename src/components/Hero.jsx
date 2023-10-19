93e634e609ca4d6771374f046c5ca97a3c46788d
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2023-10-19 07:51:19 +0530

    style:search button padding changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,14 +7,14 @@ import tick from "../assets/Vectors/tick.svg"
 
 const Hero = () => {
   return (
-    <section className="relative px-4 py-8">
+    <section className="relative px-2 sm:px-4 py-8 w-full">
             <div className="hidden xl:block absolute w-72 h-72 rotate-[25deg] -left-16 -top-14  z-0">
               <img src={leaf} alt="leaf" className="p-6 blur-sm" />
             </div>
 
-            <div className=" flex flex-wrap md:justify-between items-center space-y-6 ">
+            <div className=" flex flex-wrap px-2 md:px-0 justify-center md:justify-between items-center space-y-6 ">
               {/* Main text */}
-              <div className="basis-full flex-grow lg:basis-3/5 relative flex flex-col justify-center items-center lg:items-start gap-4 md:gap-7 z-10">
+              <div className="basis-full flex-grow lg:basis-3/5 relative flex flex-col lg:justify-center items-center lg:items-start gap-y-4 md:gap-y-7 z-10">
                 <h1 className=" max-w-2xl text-center sm:text-center lg:text-start text-5xl md:text-6xl font-Marienda text-primary font-bold">
                   Order food online from your favourite local restaurants
                   <span className="text-secondary">.</span>
@@ -25,7 +25,7 @@ const Hero = () => {
                 </p>
 
                 <div className="max-w-3xl self-center xl:self-auto w-full flex box-border border border-r-0 rounded-[6rem]">
-                  <div className="flex-1 flex gap-2 sm:gap-4 pl-2 py-2  items-center">
+                  <div className="flex-1 flex sm:gap-4 pl-2 py-2  items-center">
                     <div className="bg-secondary p-2 rounded-full hidden sm:block">
                       <img src={mark} alt="mark" className="w-6 h-6" />
                     </div>
@@ -39,7 +39,7 @@ const Hero = () => {
                   </div>
 
                  
-                  <button className="search__btn bg-primary">
+                  <button className="inline-flex justify-center items-center rounded-r-full rounded-l-full gap-x-2 bg-primary px-4 py-3 ">
                     <img src={search} alt="search" className='hidden sm:block' />
                     <span className="text-white  font-Poppins">Search</span>
                   </button>
@@ -47,7 +47,7 @@ const Hero = () => {
               </div>
 
               {/* Food Plate */}
-              <div className="basis-full shrink-0 flex-grow lg:basis-2/5 max-w-lg relative mx-auto">
+              <div className="basis-full sm:shrink-0 flex-grow lg:basis-2/5 max-w-lg relative mx-auto">
                 {/* glassmorphism-effect-card */}
                 <div className="glass__btn border">
                   <div className="flex flex-col items-center">
